4528334c4ab3f68e87ae329d67113269d8dd9353
pandeyoga <136729349+pandeyoga@users.noreply.github.com>
2023-06-16 22:13:30 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 from bokeh.embed import server_document
 
 # Get the script and div for the Bokeh application
-bokeh_script = server_document('http://localhost:5006/top_spotify_song_app')
+bokeh_script = server_document('https://raw.githubusercontent.com/pandeyoga/visdat/main/spotify.py')
 
 # Render the Bokeh application in Streamlit
 st.bokeh_chart(bokeh_script, use_container_width=True)
